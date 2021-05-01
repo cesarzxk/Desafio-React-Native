3fce587be5383f1b12b331440a2d97e033da6b82
César Vargas Dos Santos <43748428+cesarzxk@users.noreply.github.com>
2021-05-01 19:22:43 -0300

    Buttons & Modal implemented.
    All buttons and the modal component have been implemented.

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,70 @@
+import React,{useContext, useState} from 'react';
+import { FontAwesome } from '@expo/vector-icons'; 
+import { GlobalContext } from '../../context';
+
+
+import {
+    ContainerModal,
+    Fade,
+    ButtonPost,
+    ButtonPostText,
+    ContainerButtonPost,
+    ContainerPostBox,
+    ContainerPostTextBox,
+    ContainerTitleTextBox,
+    ContainerTrashIcon,
+    ContainerUserTextBox,
+    Text,
+    TitleTextBox,
+    UserTextBox,
+    PostTextBox,
+    Header
+} from './styled';
+
+
+export default function Modal(){
+    const {isVisible,setIsVisible, addPost} = useContext(GlobalContext);
+    const [title, setTitle] = useState<string>('');
+    const [body, setBody] = useState<string>('');
+    const [userName, setUserName] = useState<string>('');
+
+    function clearData(){
+        setUserName('');
+        setBody('');
+        setTitle('');
+        setIsVisible(false)
+    }
+
+
+    return(
+        <ContainerModal transparent={true} visible={isVisible} animationType="slide">
+                    <Fade>
+                        <ContainerPostBox>
+                            <Header/>
+                            <ContainerTrashIcon onPress={()=>{clearData()}}>
+                                <FontAwesome name="trash-o" size={24} color="black" /></ContainerTrashIcon>
+                            <ContainerUserTextBox>
+                                <Text>User:</Text>
+                                <UserTextBox value={userName} onChangeText={text =>setUserName(text)}/>
+                            </ContainerUserTextBox>
+
+                            <ContainerTitleTextBox>
+                                <Text>Title:</Text>
+                                <TitleTextBox value={title} onChangeText={text =>setTitle(text)}/>
+                            </ContainerTitleTextBox>
+                            
+                            <ContainerPostTextBox>
+                                <Text>Post:</Text>
+                                <PostTextBox value={body} onChangeText={text =>setBody(text)} multiline = {true}/>
+                            </ContainerPostTextBox>
+
+                            <ContainerButtonPost>
+                                <ButtonPost onPress={()=>{addPost(userName,title,body); clearData()}}>
+                                    <ButtonPostText>Post</ButtonPostText>
+                                </ButtonPost>
+                            </ContainerButtonPost>
+                        </ContainerPostBox>
+                    </Fade>
+        </ContainerModal>
+        )
+}
